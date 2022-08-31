e5203cc7382e42f5c9783729a9200917ae2f0634
menna1993 <mennaaboulsaadat@gmail.com>
2022-08-31 18:17:27 +0200

    changes

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -18,11 +18,11 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 80;
 
   app.use(bodyParser.json());
 
-  // app.use(cors());
+  app.use(cors());
   // We set the CORS origin to * so that we don't need to
   // worry about the complexities of CORS. 
   app.use(cors({
