1c2e4dd719560567ff8d61840786293d72e2ef7c
menna1993 <mennaaboulsaadat@gmail.com>
2022-09-02 14:40:03 +0200

    changed

--- udagram/setup.sh
+++ udagram/setup.sh
@@ -2,5 +2,5 @@ mkdir ~/.aws
 touch ~/.aws/config
 chmod 600 ~/.aws/config
 echo "[profile eb-cli]" > ~/.aws/config
-echo "aws_access_key_id=AKIARZSBTKJQAVNNDWWS" >> ~/.aws/config
-echo "aws_secret_access_key=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" >> ~/.aws/config
+echo "aws_access_key_id=AKIARZSBTKJQBRP6WBGZ" >> ~/.aws/config
+echo "aws_secret_access_key=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" >> ~/.aws/config
